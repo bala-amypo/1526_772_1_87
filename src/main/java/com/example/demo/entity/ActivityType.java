ed0576cab54de3c09f55c313efd0572af9286e8c
bala-amypo <bala@amypo.in>
2025-12-18 09:27:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/ActivityType.java
@@ -0,0 +1,46 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "activity_types")
+public class ActivityType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String typeName;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ActivityCategory category;
+
+    public ActivityType() {}
+
+    // getters & setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public ActivityCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ActivityCategory category) {
+        this.category = category;
+    }
+}
