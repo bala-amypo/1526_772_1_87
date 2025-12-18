7f3f07fdb430add5da54771ee550816dcab6b0da
bala-amypo <bala@amypo.in>
2025-12-18 13:06:28 +0000

    User commit

--- src/main/java/com/example/repository/EmissionFactorRepository.java
+++ src/main/java/com/example/repository/EmissionFactorRepository.java
@@ -4,10 +4,7 @@ import com.example.demo.entity.EmissionFactor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface EmissionFactorRepository extends JpaRepository<EmissionFactor, Long> {
-    // Add custom method used in ActivityLogServiceImpl
-    List<EmissionFactor> findByActivityTypeId(Long activityTypeId);
+public interface EmissionFactorRepository
+        extends JpaRepository<EmissionFactor, Long> {
 }
