fad1445a3e004686a6f3527a46eb03e30fda0b71
bala-amypo <bala@amypo.in>
2025-12-29 09:11:48 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ActivityTypeServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ActivityTypeServiceImpl.java
@@ -1,17 +1,5 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
 
-import com.example.demo.repository.ActivityCategoryRepository;
-import com.example.demo.repository.ActivityTypeRepository;
-import com.example.demo.service.ActivityTypeService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ActivityTypeServiceImpl implements ActivityTypeService {
-    private final ActivityCategoryRepository categoryRepository;
-    private final ActivityTypeRepository typeRepository;
-
-    public ActivityTypeServiceImpl(ActivityCategoryRepository categoryRepository, ActivityTypeRepository typeRepository) {
-        this.categoryRepository = categoryRepository;
-        this.typeRepository = typeRepository;
-    }
+public interface ActivityTypeService {
 }
+ 
--- src/main/java/com/example/demo/service/impl/EmissionFactorServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/EmissionFactorServiceImpl.java
@@ -1,25 +1,7 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
 
 import com.example.demo.entity.EmissionFactor;
-import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.repository.ActivityTypeRepository;
-import com.example.demo.repository.EmissionFactorRepository;
-import com.example.demo.service.EmissionFactorService;
-import org.springframework.stereotype.Service;
 
-@Service
-public class EmissionFactorServiceImpl implements EmissionFactorService {
-    private final ActivityTypeRepository typeRepository;
-    private final EmissionFactorRepository factorRepository;
-
-    public EmissionFactorServiceImpl(ActivityTypeRepository typeRepository, EmissionFactorRepository factorRepository) {
-        this.typeRepository = typeRepository;
-        this.factorRepository = factorRepository;
-    }
-
-    @Override
-    public EmissionFactor getFactorByType(Long activityTypeId) {
-        return factorRepository.findByActivityType_Id(activityTypeId)
-                .orElseThrow(() -> new ResourceNotFoundException("Emission factor not found"));
-    }
+public interface EmissionFactorService {
+    EmissionFactor getFactorByType(Long activityTypeId);
 }
